6d8de068b14ddda2dfcab0ded91c1ea9b074271f
Jose Antonio Amieva <jo_amieva7@hotmail.com>
2025-01-23 14:15:58 -0600

    upgrading to tailwind v4

--- src/components/Sidebar/venues-switcher.tsx
+++ src/components/Sidebar/venues-switcher.tsx
@@ -80,7 +80,7 @@ export function VenuesSwitcher({ venues, defaultVenue }: { venues: Venue[]; defa
               </SidebarMenuButton>
             </DropdownMenuTrigger>
             <DropdownMenuContent
-              className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+              className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
               align="start"
               side={isMobile ? 'bottom' : 'right'}
               sideOffset={4}
